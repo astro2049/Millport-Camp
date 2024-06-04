2d9dd1cf9460f22a6bdf1abc6b770522e777c321
Jerry Lyu <45759373+astro2049@users.noreply.github.com>
2024-06-04 01:57:47 +0100

    shooting and tracers

--- Assets/Scripts/Player/PlayerInputComponent.cs
+++ Assets/Scripts/Player/PlayerInputComponent.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -8,12 +9,15 @@ public class PlayerInputComponent : MonoBehaviour
      */
     [Header("Movement Settings")] public float moveSpeed = 5f; // 5m/s
     public Camera followCamera;
+    // Tracer
+    public GameObject tracerPrefab;
+    public float tracerSpeed = 150f; // 150m/s
 
     /*
      * Private fields
      */
     private Vector2 moveInput;
-    private GameObject gameObjectHit;
+    private Transform muzzleTransform;
 
     /*
      * Precalculated private fields
@@ -23,7 +27,9 @@ public class PlayerInputComponent : MonoBehaviour
     // Start is called before the first frame update
     private void Start()
     {
-        // Precalculate camera forward and right vectors
+        /*
+         * Precalculate camera forward and right vectors
+         */
         cameraForward = followCamera.transform.forward;
         cameraRight = followCamera.transform.right;
         // Zero out the y components to stay on the same plane
@@ -32,6 +38,11 @@ public class PlayerInputComponent : MonoBehaviour
         // Normalize the vectors
         cameraForward.Normalize();
         cameraRight.Normalize();
+
+        /*
+         * Precalculate muzzle transform
+         */
+        muzzleTransform = transform.Find("Muzzle");
     }
 
     // Update is called once per frame
@@ -48,10 +59,11 @@ public class PlayerInputComponent : MonoBehaviour
 
     private void Move()
     {
-        if (moveInput != Vector2.zero) {
-            Vector3 moveDirection = (moveInput.y * cameraForward + moveInput.x * cameraRight).normalized;
-            transform.Translate(moveDirection * (moveSpeed * Time.deltaTime), Space.World);
+        if (moveInput == Vector2.zero) {
+            return;
         }
+        Vector3 moveDirection = (moveInput.y * cameraForward + moveInput.x * cameraRight).normalized;
+        transform.Translate(moveDirection * (moveSpeed * Time.deltaTime), Space.World);
     }
 
     private void Look()
@@ -61,28 +73,53 @@ public class PlayerInputComponent : MonoBehaviour
          * https://www.youtube.com/watch?v=0jTPKz3ga4w (Accessed 30 May 2024)
          */
         Ray ray = followCamera.ScreenPointToRay(Mouse.current.position.ReadValue());
-        if (Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity, LayerMask.GetMask("Terrain", "NPC"))) {
-            gameObjectHit = hit.collider.gameObject;
-            if (gameObjectHit.layer == LayerMask.NameToLayer("Terrain")) {
-                Debug.DrawLine(followCamera.ScreenToWorldPoint(Mouse.current.position.ReadValue()), hit.point, Color.red);
-            } else {
-                Debug.DrawLine(followCamera.ScreenToWorldPoint(Mouse.current.position.ReadValue()), hit.point, Color.green);
-            }
+        if (Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity, LayerMask.GetMask("Terrain", "Obstacle", "NPC"))) {
+            // Debug: DrawLine - Green if hit NPC, otherwise red
+            Debug.DrawLine(followCamera.ScreenToWorldPoint(Mouse.current.position.ReadValue()), hit.point, hit.collider.gameObject.layer == LayerMask.NameToLayer("NPC") ? Color.green : Color.red);
             transform.LookAt(new Vector3(
                 hit.point.x,
                 transform.position.y,
                 hit.point.z
             ));
-        } else {
-            gameObjectHit = null;
         }
     }
 
     private void OnAttack()
     {
-        if (gameObjectHit && gameObjectHit.layer == LayerMask.NameToLayer("NPC")) {
-            // Debug.Log(gameObjectHit.name);
-            GetComponent<DamageDealerComponent>().DealDamage(gameObjectHit, 100f);
+        Vector3 muzzlePosition = muzzleTransform.position;
+        Ray ray = new Ray(muzzlePosition, transform.forward);
+        TrailRenderer tracerTrail = Instantiate(tracerPrefab, muzzlePosition, Quaternion.LookRotation(transform.forward)).GetComponent<TrailRenderer>();
+        if (Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity, LayerMask.GetMask("Terrain", "Obstacle", "NPC"))) {
+            // Debug: DrawLine - Green if hit NPC, otherwise cyan
+            Debug.DrawLine(muzzlePosition, hit.point, hit.collider.gameObject.layer == LayerMask.NameToLayer("NPC") ? Color.green : Color.cyan, 5f);
+            // Tracer effect
+            StartCoroutine(SpawnTrail(tracerTrail, hit.distance));
+            if (hit.collider.gameObject.layer == LayerMask.NameToLayer("NPC")) {
+                // Deal damage
+                GetComponent<DamageDealerComponent>().DealDamage(hit.collider.gameObject, 100f);
+            }
+        } else {
+            // Tracer effect, endPoint is arbitrary (100m away, out of screen)
+            StartCoroutine(SpawnTrail(tracerTrail, 100f));
         }
+    }
+
+    /*
+     * Tracer Effect
+     * TheKiwiCoder (2020), '[#05] Shooting a weapon using Projectile Raycasts (with effects)', Youtube, 17 May
+     * https://www.youtube.com/watch?v=onpteKMsE84 (Accessed 3 June 2024)
+     * BMo (2022), 'How to Add a TRAIL EFFECT to Anything in Unity', youtube, 2 May
+     * https://www.youtube.com/watch?v=nLxvCRPJCKw (Accessed 3 June 2024)
+     */
+    private IEnumerator SpawnTrail(TrailRenderer tracerTrail, float distance)
+    {
+        float time = 0f;
+        float arrivalTime = distance / tracerSpeed;
+        while (time <= arrivalTime) {
+            tracerTrail.transform.Translate(Vector3.forward * (tracerSpeed * Time.deltaTime));
+            time += Time.deltaTime;
+            yield return null;
+        }
+        Destroy(tracerTrail.gameObject);
     }
 }
